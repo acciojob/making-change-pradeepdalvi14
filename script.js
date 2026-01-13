d370ef4f69b3e85f12b35db1cede3929176ac649
Pradeep <157382496+pradeepdalvi14@users.noreply.github.com>
2026-01-13 21:37:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 const makeChange = (c) => {
-  // your name here
+   let num = Number(c); // ensure it's a numbe
+
+  let q = Math.floor(num / 25);
+  num = num % 25;
+
+  let d = Math.floor(num / 10);
+  num = num % 10;
+
+  let n = Math.floor(num / 5);
+  num = num % 5;
+
+  let p = Math.floor(num / 1);
+
+  return {
+    q: q,
+    d: d,
+    n: n,
+    p: p
+  };
 };
 
 // Do not the change the code below
